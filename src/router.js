3959f5a6e754f1ea74cbfc501d1ad820631f4dec
aoligei <7751563+wikooo@user.noreply.gitee.com>
2022-06-12 19:16:13 +0800

    修改结构

--- src/router.js
+++ src/router.js
@@ -3,7 +3,7 @@ import VueRouter from 'vue-router'
 import Login from './components/Login.vue'
 import Home from './components/Home.vue'
 import Welcome from './components/Welcome.vue'
-import Users from './components/User.vue'
+import Users from './components/user/User.vue'
 
 Vue.use(VueRouter)
 
