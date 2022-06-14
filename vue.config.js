d28210c63906e960c4316bbe5c6087fa9d7c670c
aoligei <7751563+wikooo@user.noreply.gitee.com>
2022-06-14 21:01:11 +0800

    模拟

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,17 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
-  publicPath: './'
+  publicPath: './',
+  chainWebpack: config => {
+    // 开发和发布模式
+    config.when(process.env.NODE_ENV === 'production', config => {
+      config
+        .entry('app')
+        .clear()
+        .add('./src/main-prod.js')
+    })
+    config.when(process.env.NODE_ENV === 'development', config => {
+      config.entry('app').clear().add('./src/main-dev.js')
+    })
+  }
 })
